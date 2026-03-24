3aa66aa21c886052e6bdc687278e82b2214fa96e
okyproxd <owenkentyu@gmail.com>
2026-03-24 11:57:31 +0000

    Update app_menu.c

--- app/src/app_menu.c
+++ app/src/app_menu.c
@@ -1,31 +1,46 @@
 #include <furi.h>
 #include <gui/gui.h>
+#include <gui/modules/submenu.h>
+#include "app_menu.h"
 #include "bluetooth_monitor.h"
 #include "usb_monitor.h"
 
-void main_menu_callback(uint32_t index) {
-    switch (index) {
-        case 0:
-            start_bluetooth_monitoring();
-            break;
-        case 1:
-            start_usb_monitoring();
-            break;
-        default:
-            furi_log("Invalid menu option");
-            break;
+typedef enum {
+    SubmenuIndexBluetooth,
+    SubmenuIndexUSB,
+} SubmenuIndex;
+
+void submenu_callback(void* context, uint32_t index) {
+    UNUSED(context);
+    if(index == SubmenuIndexBluetooth) {
+        start_bluetooth_monitoring();
+    } else if(index == SubmenuIndexUSB) {
+        start_usb_monitoring();
     }
 }
 
 void app_menu_init() {
-    ViewPort* viewport = view_port_alloc();
     Gui* gui = furi_record_open(RECORD_GUI);
+    Submenu* submenu = submenu_alloc();
+    
+    submenu_set_header(submenu, "Keystroke Monitor");
+    submenu_add_item(submenu, "Bluetooth Monitor", SubmenuIndexBluetooth, submenu_callback, NULL);
+    submenu_add_item(submenu, "USB Monitor", SubmenuIndexUSB, submenu_callback, NULL);
+    
+    ViewPort* viewport = submenu_get_view_port(submenu);
     gui_add_view_port(gui, viewport, GuiLayerFullscreen);
 
-    const char* menu_items[] = {"Bluetooth Monitoring", "USB Monitoring"};
-    uint32_t selected_option = gui_show_menu("Keystroke Monitor", menu_items, 2);
-    main_menu_callback(selected_option);
+    // Wait for user to exit via back button
+    bool running = true;
+    while(running) {
+        furi_delay_ms(100);
+        // Simple exit condition for this rewrite
+        if(furi_hal_gpio_read(&gpio_button_back) == false) {
+            running = false;
+        }
+    }
 
-    view_port_free(viewport);
+    gui_remove_view_port(gui, viewport);
+    submenu_free(submenu);
     furi_record_close(RECORD_GUI);
 }
